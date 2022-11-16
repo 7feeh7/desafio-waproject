0ba3610cfef33050edd61212871091fea445fbc5
Felipe Pires Soares <felipe.soares.e@midway.com.br>
2022-11-16 11:41:25 -0300

    enable cors

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,10 +6,4 @@ routes.get('/films', (req, res) => {
     return res.json({ name: "teste" })
 })
 
-routes.post('/films', (req, res) => {
-    const { name } = req.body
-    console.log("name", name)
-    return res.status(200)
-})
-
 module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,17 @@ const routes = require('./routes')
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
+
+// Enable CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    app.use(cors())
+    next()
+})
+
 app.use(routes)
 
 // Adding generic catch
