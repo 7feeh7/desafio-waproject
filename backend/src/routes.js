2142d9d80a4b5af549967fd651fa0228a0dd58f7
Felipe Pires Soares <felipe.soares.e@midway.com.br>
2022-11-16 12:26:16 -0300

    consuming external API

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const axios = require('axios')
 
 const routes = express.Router()
 
-routes.get('/films', (req, res) => {
-    return res.json({ name: "teste" })
+const apiFilms = 'https://ghibliapi.herokuapp.com/films'
+
+routes.get('/films', async (req, res) => {
+    const response = await axios.get(apiFilms)
+
+    const films = response.data
+
+    for (const film of films) {
+        const { title, description, director, producer } = film
+        console.log('title', title)
+        console.log('description', description)
+        console.log('director', director)
+        console.log('producer', producer)
+    }
+    
+    return res.send("Searching")
 })
 
 module.exports = routes
