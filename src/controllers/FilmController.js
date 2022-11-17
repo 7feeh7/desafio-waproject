c3393a07fbac7716c9a2b242865a2c5b592c881b
Felipe Pires Soares <felipe.soares.e@midway.com.br>
2022-11-16 21:37:20 -0300

    adding total films in the request

--- src/controllers/FilmController.js
+++ src/controllers/FilmController.js
@@ -3,12 +3,16 @@ const Film = require('../models/Film');
 module.exports = {
     async index(req, res) {
         const { page = 1 } = req.query;
-       
-        const perPage = 10;
-       
+        
+        const perPage = 10
+        
         const films = await Film.find()
             .skip((page-1)*perPage)
             .limit(perPage);
+        
+        const count = await Film.find().count();
+        
+        res.header('X-Total-Count', count);
         
         return res.status(200).json(films);
     }
