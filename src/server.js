8b0b075321f4bb3e0f2afd266446c0e88590acb1
Felipe Pires Soares <felipe.soares.e@midway.com.br>
2022-11-21 13:51:11 -0300

    fix env

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
-require('dotenv').config({
-    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
-});
+require('dotenv').config();
 
 const app = express();
 
